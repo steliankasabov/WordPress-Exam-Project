85692b67e33b667c9ed479c4cd1915bd18fc769d
Stelian Kasabov <steliankasabov@users.noreply.github.com>
2024-02-11 17:17:47 +0200

    Boxus Theme

---
+++ wp-content/themes/softuni/header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE HTML> 
+<html lang="en-US">
+    <head>
+        <title>Boxus - Html Responsive One Page Template</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="description" content="Template by Colorlib" />
+        <meta name="keywords" content="HTML, CSS, JavaScript, PHP" />
+        <meta name="author" content="Colorlib" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+        <link rel="shortcut icon" href="http://localhost/softuni/wp-content/themes/softuni/images/favicon.png" />
+        <link href='http://fonts.googleapis.com/css?family=Roboto:300,400,400i,700,700i,900|Montserrat:400,700|PT+Serif' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" type="text/css"  href='http://localhost/softuni/wp-content/themes/softuni/css/clear.css' />
+        <link rel="stylesheet" type="text/css"  href='http://localhost/softuni/wp-content/themes/softuni/css/common.css' />
+        <link rel="stylesheet" type="text/css"  href='http://localhost/softuni/wp-content/themes/softuni/css/font-awesome.min.css' />
+        <link rel="stylesheet" type="text/css"  href='http://localhost/softuni/wp-content/themes/softuni/css/carouFredSel.css' />
+        <link rel="stylesheet" type="text/css"  href='http://localhost/softuni/wp-content/themes/softuni/css/prettyPhoto.css' />
+        <link rel="stylesheet" type="text/css"  href='http://localhost/softuni/wp-content/themes/softuni/css/sm-clean.css' />
+        <link rel="stylesheet" type="text/css"  href='http://localhost/softuni/wp-content/themes/softuni/style.css' />
+
+        <!--[if lt IE 9]>
+                <script src="js/html5.js"></script>
+        <![endif]-->
+
+    </head>
+    <body>
+
+        <!-- Preloader Gif -->
+        <table class="doc-loader">
+            <tr>
+                <td>
+                    <img src="http://localhost/softuni/wp-content/themes/softuni/images/ajax-document-loader.gif" alt="Loading..." />
+                </td>
+            </tr>
+        </table>
+
+        <!-- Menu -->
+        <div class="menu-wrapper center-relative">
+            <nav id="header-main-menu">
+                <div class="mob-menu">MENU</div>
+                <ul class="main-menu sm sm-clean">
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#services">Services</a></li>
+                    <li><a href="#portfolio">Portfolio</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#news">News</a></li>
+                    <li><a href="#video">Video</a></li>
+                    <li><a href="#skills">Skills</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
