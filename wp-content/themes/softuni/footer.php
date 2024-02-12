274323a4bb568dcdd9b1aa05a9a89e8acbf342f4
Stelian Kasabov <steliankasabov@users.noreply.github.com>
2024-02-12 23:31:18 +0200

    header.php and foother.php

---
+++ wp-content/themes/softuni/footer.php
@@ -0,0 +1,38 @@
+        <!-- Footer -->
+
+        <footer>
+            <div class="footer content-1170 center-relative">
+                <ul>
+                    <li class="copyright-footer">
+                        © <?php echo date('Y'); ?> All rights reserved. | Boxus Template by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                    </li>
+                    <li class="social-footer">
+                        <a href="#"><span class="fa fa-twitter"></span></a>
+                        <a href="#"><span class="fa fa-behance"></span></a>
+                        <a href="#"><span class="fa fa-dribbble"></span></a>
+                        <a href="#"><span class="fa fa-facebook"></span></a>
+                        <a href="#"><span class="fa fa-rss"></span></a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+
+
+        <!--Load JavaScript-->
+        <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.js"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.js"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.sticky-kit.min.js"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.smartmenus.min.js"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.sticky.js"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.dryMenu.js"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>/js/isotope.pkgd.js"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.carouFredSel-6.2.0-packed.js"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.mousewheel.min.js"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.touchSwipe.min.js"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.easing.1.3.js"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>/js/imagesloaded.pkgd.js"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.prettyPhoto.js"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
+
+    </body>
+</html>
--- wp-content/themes/softuni/header.php
+++ wp-content/themes/softuni/header.php
@@ -1,22 +1,22 @@
 <!DOCTYPE HTML> 
-<html lang="en-US">
+<html <?php language_attributes(); ?>>
     <head>
-        <title>Boxus - Html Responsive One Page Template</title>
+        <title><?php wp_title('|', true, 'right'); ?></title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta name="description" content="Template by Colorlib" />
         <meta name="keywords" content="HTML, CSS, JavaScript, PHP" />
         <meta name="author" content="Colorlib" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-        <link rel="shortcut icon" href="http://localhost/softuni/wp-content/themes/softuni/images/favicon.png" />
+        <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.png" />
         <link href='http://fonts.googleapis.com/css?family=Roboto:300,400,400i,700,700i,900|Montserrat:400,700|PT+Serif' rel='stylesheet' type='text/css'>
-        <link rel="stylesheet" type="text/css"  href='http://localhost/softuni/wp-content/themes/softuni/css/clear.css' />
-        <link rel="stylesheet" type="text/css"  href='http://localhost/softuni/wp-content/themes/softuni/css/common.css' />
-        <link rel="stylesheet" type="text/css"  href='http://localhost/softuni/wp-content/themes/softuni/css/font-awesome.min.css' />
-        <link rel="stylesheet" type="text/css"  href='http://localhost/softuni/wp-content/themes/softuni/css/carouFredSel.css' />
-        <link rel="stylesheet" type="text/css"  href='http://localhost/softuni/wp-content/themes/softuni/css/prettyPhoto.css' />
-        <link rel="stylesheet" type="text/css"  href='http://localhost/softuni/wp-content/themes/softuni/css/sm-clean.css' />
-        <link rel="stylesheet" type="text/css"  href='http://localhost/softuni/wp-content/themes/softuni/style.css' />
+        <link rel="stylesheet" type="text/css"  href='<?php echo get_template_directory_uri(); ?>/css/clear.css' />
+        <link rel="stylesheet" type="text/css"  href='<?php echo get_template_directory_uri(); ?>/css/common.css' />
+        <link rel="stylesheet" type="text/css"  href='<?php echo get_template_directory_uri(); ?>/css/font-awesome.min.css' />
+        <link rel="stylesheet" type="text/css"  href='<?php echo get_template_directory_uri(); ?>/css/carouFredSel.css' />
+        <link rel="stylesheet" type="text/css"  href='<?php echo get_template_directory_uri(); ?>/css/prettyPhoto.css' />
+        <link rel="stylesheet" type="text/css"  href='<?php echo get_template_directory_uri(); ?>/css/sm-clean.css' />
+        <link rel="stylesheet" type="text/css"  href='<?php echo get_template_directory_uri(); ?>/style.css' />
 
         <!--[if lt IE 9]>
                 <script src="js/html5.js"></script>
@@ -29,7 +29,7 @@
         <table class="doc-loader">
             <tr>
                 <td>
-                    <img src="http://localhost/softuni/wp-content/themes/softuni/images/ajax-document-loader.gif" alt="Loading..." />
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/ajax-document-loader.gif" alt="Loading..." />
                 </td>
             </tr>
         </table>
